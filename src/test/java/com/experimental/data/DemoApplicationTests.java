44e356f7c0dd464483c2d82dc7f96f8d22a93d1a
Sai Rohith Reddy Vangala <sairohith2605@outlook.com>
2020-01-10 10:29:55 +0530

    Created Application for Database Ops

---
+++ src/test/java/com/experimental/data/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experimental.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
